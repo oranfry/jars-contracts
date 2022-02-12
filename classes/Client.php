0be52f044d21554daeb703d3dfd5999f7b29545e
Oran Fry <oran@emerg.co>
2022-02-13 00:47:59 +1300

    Client Contract
    Put into a deeper namespace. Change to include things that were recently removed, and remove one required function.

--- classes/Client.php
+++ classes/Client.php
@@ -1,17 +1,18 @@
 <?php
 
-namespace jars;
+namespace jars\contract;
 
 interface Client
 {
     public function delete($linetype, $id);
     public function fields($linetype);
     public function get($linetype, $id);
+    public function group(string $report, string $group, ?string $min_version = null);
+    public function groups(string $report, ?string $min_version = null);
     public function login(string $username, string $password);
     public function logout();
     public function preview(array $data);
     public function record($table, $id);
-    public function report(string $name);
     public function save(array $data, bool $dryrun = false);
     public function touch();
     public function unlink($linetype, $id, $parent);
