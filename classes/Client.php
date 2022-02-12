a0e93c0a5ca9dd92ee199cdf77543e2af8c76cdf
Oran Fry <oran@emerg.co>
2022-02-12 15:05:40 +1300

    Reports
    Change how reports and their data will be fetched.

--- classes/Client.php
+++ classes/Client.php
@@ -7,12 +7,11 @@ interface Client
     public function delete($linetype, $id);
     public function fields($linetype);
     public function get($linetype, $id);
-    public function groups(string $name, ?string $min_version = null);
     public function login(string $username, string $password);
     public function logout();
     public function preview(array $data);
     public function record($table, $id);
-    public function report(string $name, string $group, ?string $min_version = null);
+    public function report(string $name);
     public function save(array $data, bool $dryrun = false);
     public function touch();
     public function unlink($linetype, $id, $parent);
