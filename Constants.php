4ea34c44766e0c609bf904d4af50dce4bdd42094
Oran Fry <oran@emerg.co>
2023-09-02 11:49:50 +1200

    Groups
    Add empty string to the set of valid group names.

--- Constants.php
+++ Constants.php
@@ -7,5 +7,5 @@ class Constants
     const GROUP_BARREL_PATTERN = '[a-zA-Z0-9]+';
     const GROUP_SUB_PATTERN = '(?:' . self::GROUP_BARREL_PATTERN . ')(?:-(?:' . self::GROUP_BARREL_PATTERN . '))*';
     const GROUP_PREFIX_PATTERN = '(?:' . self::GROUP_SUB_PATTERN . '\\/)*';
-    const GROUP_PATTERN = '(?:' . self::GROUP_PREFIX_PATTERN . ')(?:' . self::GROUP_SUB_PATTERN . ')';
+    const GROUP_PATTERN = '(?:(?:' . self::GROUP_PREFIX_PATTERN . ')(?:' . self::GROUP_SUB_PATTERN . '))?';
 }
