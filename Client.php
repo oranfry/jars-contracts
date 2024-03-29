ce9985f445547bd9dabe78fbda54d11d3790c70c
Oran Fry <oran@emerg.co>
2024-03-29 21:17:49 +1300

    Min Versions
    Allow passing true as min version, meaning the latest version known to the client.

--- Client.php
+++ Client.php
@@ -7,8 +7,8 @@ interface Client
     public function delete(string $linetype, string $id): array;
     public function fields(string $linetype): array;
     public function get(string $linetype, string $id): ?object;
-    public function group(string $report, string $group = '', ?string $min_version = null);
-    public function groups(string $report, string $prefix = '', ?string $min_version = null): array;
+    public function group(string $report, string $group = '', string|bool|null $min_version = null);
+    public function groups(string $report, string $prefix = '', string|bool|null $min_version = null): array;
     public function h2n(string $h): ?int;
     public function linetypes(?string $report = null): array;
     public function login(string $username, string $password): ?string;
@@ -20,7 +20,7 @@ interface Client
     public function refresh(): string;
     public function reports(): array;
     public function save(array $lines): array;
-    public function token(?string $token = null): null|string|self;
+    public function token(?string $token = null): self|string|null;
     public function touch(): object;
     public function version(): ?string;
 }
