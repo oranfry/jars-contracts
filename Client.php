e9fb9207e1ac79f9262069168d6330394e8e8cb2
Oran Fry <oran@emerg.co>
2026-01-04 14:28:11 +1300

    Parameter Names
    Append _name to certain parameters in case the implementation wants to use the bare name for an object deviating from the interface variable name.

--- Client.php
+++ Client.php
@@ -4,21 +4,21 @@ namespace jars\contract;
 
 interface Client
 {
-    public function delete(string $linetype, string $id): array;
-    public function fields(string $linetype): array;
+    public function delete(string $linetype_name, string $id): array;
+    public function fields(string $linetype_name): array;
     public function flatten(?object $object = null): ?object;
-    public function get(string $linetype, string $id): ?object;
-    public function group(string $report, string $group = '', string|bool|null $min_version = null);
-    public function groups(string $report, string $prefix = '', string|bool|null $min_version = null): array;
+    public function get(string $linetype_name, string $id): ?object;
+    public function group(string $report_name, string $group = '', string|bool|null $min_version = null);
+    public function groups(string $report_name, string $prefix = '', string|bool|null $min_version = null): array;
     public function h2n(string $h): ?int;
     public function info(?string $varname = null): array|string|null;
-    public function linetypes(?string $report = null): array;
+    public function linetypes(?string $report_name = null): array;
     public function login(?string $username = null, ?string $password = null): ?string;
     public function logout(): bool;
     public function n2h(int $n): string;
     public function persist(): self;
     public function preview(array $lines, ?string $base_version = null): array;
-    public function record(string $table, string $id, ?string &$content_type = null, ?string &$filename = null): ?string;
+    public function record(string $table_name, string $id, ?string &$content_type = null, ?string &$filename = null): ?string;
     public function reports(): array;
     public function save(array $lines, ?string $base_version = null): array;
     public function token(?string $token = null): self|string|null;
