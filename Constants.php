825b467ffb0946f74fd3eae692bb5c4b456913a2
Oran Fry <oran@emerg.co>
2026-05-12 09:12:07 +1200

    Root Version
    Port the root version here so it can be easily accessed by any package.

--- Constants.php
+++ Constants.php
@@ -8,4 +8,5 @@ class Constants
     const GROUP_SUB_PATTERN = '(?:' . self::GROUP_BARREL_PATTERN . ')(?:-(?:' . self::GROUP_BARREL_PATTERN . '))*';
     const GROUP_PREFIX_PATTERN = '(?:' . self::GROUP_SUB_PATTERN . '\\/)*';
     const GROUP_PATTERN = '(?:(?:' . self::GROUP_PREFIX_PATTERN . ')(?:' . self::GROUP_SUB_PATTERN . '))?';
+    const ROOT_VERSION = '00000096d746ac3688c7de4ed14988c6ac0af8244b42a6c48298d9fff331c701';
 }
